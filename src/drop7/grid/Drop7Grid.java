f7fb1b10b0c20bbda170cb65bc5f6dfacbc3edb5
Cameron Lane <lane.cameron@gmail.com>
2012-04-18 15:02:18 -0400

    Basic methods for evaluation game bsed grid positions

--- src/drop7/grid/Drop7Grid.java
+++ src/drop7/grid/Drop7Grid.java
@@ -2,6 +2,7 @@ package drop7.grid;
 
 import java.util.ArrayList;
 
+import drop7.Drop7Constants;
 import drop7.disks.GrayDisk;
 import drop7.disks.NumberDisk;
 import info.gridworld.actor.Actor;
@@ -9,7 +10,7 @@ import info.gridworld.grid.BoundedGrid;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
-public class Drop7Grid<Disk> extends BoundedGrid<Actor> {
+public class Drop7Grid<Disk> extends BoundedGrid<Actor> implements Drop7Constants{
 
 	public Drop7Grid(int rows, int cols) {
 		super(rows,cols);
@@ -24,5 +25,18 @@ public class Drop7Grid<Disk> extends BoundedGrid<Actor> {
 		ArrayList<Location> locs = this.getOccupiedLocations();
 		
 		return this.getClass().toString() + " - " + locs.size() + " Actors in Grid";
+	}
+
+	public int getNumberOfEmptyLocations() {
+		return NUM_ROWS * NUM_COLS - this.getOccupiedLocations().size() + 5;
+	}
+
+	public boolean isBufferRowEmpty() {
+		System.out.println("got here");
+		for (Location loc : this.getOccupiedLocations()){
+			if (loc.getRow() == 1)
+				return false;
+		}
+		return true;
 	}
 }
