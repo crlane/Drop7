5f0946d92a6e751f65b75040ee91873da99e5a0f
Cameron Lane <lane.cameron@gmail.com>
2012-04-18 15:01:00 -0400

    updated constants file to include multipler

--- src/drop7/Drop7Constants.java
+++ src/drop7/Drop7Constants.java
@@ -2,9 +2,11 @@ package drop7;
 
 public interface Drop7Constants {
 	
-	public static final int NUM_ROWS = 8;
+	public static final int NUM_ROWS = 9;
 	public static final int NUM_COLS = 7;
 	public static final int NUM_STARTING_DISKS = 12;
 	public static final int HITS_TO_UNLOCK = 4;
+	// 50x^1.4 + 150
+	public static final double CASCADE_MULTIPLIER = 1.2;
 	
 }
