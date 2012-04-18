56d6a35579965fe7039f710f6135262f3b215959
Cameron Lane <lane.cameron@gmail.com>
2012-04-18 15:02:55 -0400

    Added clickedLocation code to add new disks.

--- src/drop7/world/Drop7World.java
+++ src/drop7/world/Drop7World.java
@@ -14,17 +14,23 @@ import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
-public class Drop7World<Disk> extends World<Actor> implements Drop7Constants{
+public class Drop7World<Disk> extends World<Actor> implements Drop7Constants {
 
 	@SuppressWarnings("unchecked")
-	
 	public Drop7World(Grid<Actor> grid) {
 		super(grid);
 		this.setMessage("Drop7");
-//		System.setProperty("info.gridworld.gui.selection", "hide");
-//		System.setProperty("info.gridworld.gui.tooltips", "hide");
-//		System.setProperty("info.gridworld.gui.watermark", "hide");
+		// System.setProperty("info.gridworld.gui.selection", "hide");
+		// System.setProperty("info.gridworld.gui.tooltips", "hide");
+		// System.setProperty("info.gridworld.gui.watermark", "hide");
 
 	}
-}
 
+	public boolean locationClicked(Location loc) {
+		GrayDisk d = new GrayDisk();
+		d.putSelfInGrid(this.getGrid(), loc);
+		d.moveToBottom();
+		return false;
+	}
+
+}
