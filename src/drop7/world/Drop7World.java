49f7882a8449215b7e5e46af2fa0419d78221ea6
Cameron Lane <lane.cameron@gmail.com>
2012-04-18 20:55:32 -0400

    updated package info for framework

--- src/drop7/world/Drop7World.java
+++ src/drop7/world/Drop7World.java
@@ -1,18 +1,10 @@
 package drop7.world;
 
-import java.awt.Color;
-
 import drop7.Drop7Constants;
-import drop7.disks.Disk;
-import drop7.disks.GrayDisk;
-import drop7.disks.NumberDisk;
-import drop7.grid.Drop7Grid;
-import info.gridworld.actor.Actor;
-import info.gridworld.actor.ActorWorld;
-import info.gridworld.grid.AbstractGrid;
-import info.gridworld.grid.Grid;
-import info.gridworld.grid.Location;
-import info.gridworld.world.World;
+import drop7.actor.Actor;
+import drop7.actor.GrayDisk;
+import drop7.grid.Grid;
+import drop7.grid.Location;
 
 public class Drop7World<Disk> extends World<Actor> implements Drop7Constants {
 
@@ -20,9 +12,10 @@ public class Drop7World<Disk> extends World<Actor> implements Drop7Constants {
 	public Drop7World(Grid<Actor> grid) {
 		super(grid);
 		this.setMessage("Drop7");
-		// System.setProperty("info.gridworld.gui.selection", "hide");
-		// System.setProperty("info.gridworld.gui.tooltips", "hide");
-		// System.setProperty("info.gridworld.gui.watermark", "hide");
+		System.setProperty("info.gridworld.gui.selection", "hide");
+		System.setProperty("info.gridworld.gui.tooltips", "hide");
+		System.setProperty("info.gridworld.gui.watermark", "hide");
+		System.setProperty("info.gridworld.gui.frametitle", "Drop7 GridWorld");
 
 	}
 
